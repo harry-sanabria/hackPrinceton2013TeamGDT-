571e5e25405044f7b3155f461ea7485f21151fbd
unknown <Yokipi@.(none)>
2013-11-09 04:05:29 -0500

    ESCALATE ALL THE PRIVILEGES

--- GroupBuy/app/controllers/application_controller.rb
+++ GroupBuy/app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
+  # Troll SSL bypass that makes everything super vulnerable (I think)
+  require 'openssl'
+  OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+  
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
