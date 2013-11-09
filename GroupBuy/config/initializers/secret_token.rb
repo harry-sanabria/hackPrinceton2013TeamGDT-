14a8969f09d3a1676b6915ddbb861763213c999b
Harry Sanabria <sanabria@mit.edu>
2013-11-08 21:46:34 -0500

    started rails app

---
+++ GroupBuy/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GroupBuy::Application.config.secret_key_base = 'dfef6b5bf8bb4240694ba777686560b19423aa33aed1d85a5ca2b56d444abd58fe171c7f5229f10fa79df36e3722da4cb374c42db697c761aeec9c8501d296d3'
---
+++ GroupBuy/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GroupBuy::Application.config.session_store :cookie_store, key: '_GroupBuy_session'
