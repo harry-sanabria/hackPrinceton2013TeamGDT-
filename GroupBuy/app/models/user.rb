c0594868d87ddc85d0fcf0a66744e8e48d7196dc
Charlotte <charlotte.zhu@hotmail.com>
2013-11-09 03:09:25 -0500

    idk...?

--- GroupBuy/app/models/user.rb
+++ GroupBuy/app/models/user.rb
@@ -17,12 +17,15 @@ class User < ActiveRecord::Base
     self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
   end
 
-  def self.authenticate(username, password)
-    user = find_by_username(username)
-    if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
-      user
-    else
-      nil
+  def self.authenticate(auth)
+    where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.name = auth.info.name
+      user.username = auth.info.email
+      user.oauth_token = auth.credentials.token
+      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      user.save!
     end
   end
 end
