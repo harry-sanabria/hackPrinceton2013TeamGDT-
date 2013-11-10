234e9f74367ad636277074ef90a45254a27e75a5
Harry Sanabria <sanabria@mit.edu>
2013-11-10 03:15:49 -0500

    heroku fix

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
+config.assets.initialize_on_precompile = false
+
 module GroupBuy
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -20,6 +22,5 @@ module GroupBuy
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.assets.initialize_on_precompile = false
   end
 end
