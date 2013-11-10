f2c80aab9613481ec9de15314236497e1eae8629
Harry Sanabria <sanabria@mit.edu>
2013-11-10 03:21:37 -0500

    heroku fix

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env)
+Bundler.require *Rails.groups(:assets)
 
 module GroupBuy
   class Application < Rails::Application
@@ -16,10 +16,10 @@ module GroupBuy
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
-    config.assets.initialize_on_precompile = false
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.assets.initialize_on_precompile = false
   end
 end
