1caf844c621a4a5b0f2dde64db1562aedd4af264
Harry Sanabria <sanabria@mit.edu>
2013-11-10 03:47:44 -0500

    trying desperately to get heroku to work

--- config/initializers/global_constants.rb
+++ config/initializers/global_constants.rb
@@ -1,3 +1,3 @@
-::VENMO_CLIENT_ID = '1476'
-::VENMO_CLIENT_SECRET = 'rVxpfXw9ZjvHeKzD4a3mRugfDfQP9x57'
+::VENMO_CLIENT_ID = '1487'
+::VENMO_CLIENT_SECRET = '2XFD8wRXfRhPkWerzswYkYtDH7DMMC3Y'
 ::DOMAIN = 'http://combuyne.herokuapp.com/'
