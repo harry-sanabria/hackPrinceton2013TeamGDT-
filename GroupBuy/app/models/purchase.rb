6b2a2cbd8d57743ce04a3b08b68f5c69e3ef63f0
Yolanda Yeh <yoyeh@princeton.edu>
2013-11-09 21:38:48 -0500

    show purchases for current user

--- GroupBuy/app/models/purchase.rb
+++ GroupBuy/app/models/purchase.rb
@@ -5,10 +5,17 @@ class Purchase < ActiveRecord::Base
   validates :min_price, presence: true, :numericality => {:greater_than => 0}
   validates :current_total_price, presence: true, :numericality => {:greater_than_or_equal_to => 0}
   validates :deadline, presence: true
-  attr_accessible :title, :description, :min_price, :deadline, :invited_group, :current_total_price
+  validates :user_id, presence:true
+#  validates :group, presence: true
+  attr_accessible :title, :description, :min_price, :deadline, :group, :current_total_price, :user_id
 
   def get_remaining_price()
-    return min(0, self.min_price - self.current_total_price)
+    remaining = self.min_price - self.current_total_price
+    if remaining < 0
+      return 0
+    else
+      return remaining
+    end
   end
 
   def get_current_user_payment(current_user)
@@ -19,5 +26,11 @@ class Purchase < ActiveRecord::Base
       end
     end
     return user_payment
+  end
+
+  def update_current_total_price(payment)
+    self.current_total_price = self.current_total_price + payment.price
+    self.save
+    return
   end
 end
