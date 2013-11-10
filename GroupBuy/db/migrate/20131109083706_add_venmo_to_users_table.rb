e96959e28feafa144787081688e8e65f888100c9
Yokipi <edk@u.northwestern.edu>
2013-11-09 21:47:41 -0500

    THE MAGICAL LINK WORKS Separate Venmo model created

--- GroupBuy/db/migrate/20131109083706_add_venmo_to_users_table.rb
+++
@@ -1,7 +0,0 @@
-class AddVenmoToUsersTable < ActiveRecord::Migration
-  def change
-    change_table :users do |t|
-      t.string :venmo_code
-    end
-  end
-end
--- GroupBuy/db/schema.rb
+++ GroupBuy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131109233243) do
+ActiveRecord::Schema.define(version: 20131110011908) do
 
   create_table "payments", force: true do |t|
     t.integer  "user_id"
@@ -27,11 +27,11 @@ ActiveRecord::Schema.define(version: 20131109233243) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "description"
-    t.text     "deadline"
     t.decimal  "min_price"
     t.decimal  "current_total_price"
     t.integer  "user_id"
     t.string   "group"
+    t.text     "deadline"
   end
 
   create_table "users", force: true do |t|
@@ -43,8 +43,17 @@ ActiveRecord::Schema.define(version: 20131109233243) do
     t.string   "provider"
     t.string   "uid"
     t.datetime "oauth_expires_at"
-    t.string   "venmo_code"
-    t.string   "venmo_username"
+    t.integer  "venmo_id"
+  end
+
+  create_table "venmos", force: true do |t|
+    t.integer  "user_id"
+    t.string   "username"
+    t.string   "venmo_id"
+    t.string   "refresh_code"
+    t.string   "token"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
