57d6bd13bc912cf2716a33bddd0089783674bd6c
Harry Sanabria <sanabria@mit.edu>
2013-11-10 06:26:31 -0500

    fixed typos

--- app/assets/javascripts/purchases.js
+++ app/assets/javascripts/purchases.js
@@ -1,12 +1,11 @@
-$('#layout_body').prepend('<div id="fb-root"></div>')
-alert("hello");
+$('#layout_body').prepend('<div id="fb-root"></div>');
 $(function() {
   $.ajaxSetup({ cache: true });
   $.getScript('//connect.facebook.net/en_US/all.js', function(){
     FB.init({
       appId: '389488077848048',
     });
-    facebook_token = (document.getElementById("facebook_token"))
+    facebook_token = (document.getElementById("facebook_token"));
     if (facebook_token) {
       var groups = new Array();
       FB.api('/me/groups', {access_token: facebook_token.value}, function(response) {
@@ -24,7 +23,7 @@ $(function() {
     group_id = (document.getElementById("group_id"));
     confirmation_page_token = (document.getElementById("confirmation_page_token"));
     if (confirmation_page_id && group_id && confirmation_page_token) {
-      var message = "Hey guys!  Join my payment on GroupBuy: http://combuyne.herokuapp.com/purchases/"+confirmation_page_id.value;
+      var message = "Hey guys!  Join my payment on comBUYne: http://combuyne.herokuapp.com/purchases/"+confirmation_page_id.value;
       FB.api('/'+group_id.value+'/feed', 'post', {access_token: confirmation_page_token.value, link: null, message: message}, function (response) {
         alert(response.id);
       });
